66a7eb5fb27ab0bd482f23f402fc75181e53b4a1
mixxen <alexander.cabello@gmail.com>
2025-09-19 07:59:19 -1000

    Bump version to 0.13.3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-globalThis.SATSIM_VERSION = "0.13.2";
+globalThis.SATSIM_VERSION = "0.13.3";
 
 export { default as Universe } from './engine/Universe.js'
 export { default as Event } from './engine/event/Event.js'
