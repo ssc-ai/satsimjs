b701f444ced01d7373c31946224db3eccf61d61e
mixxen <alexander.cabello@gmail.com>
2026-03-09 06:22:07 -1000

    fix: version to 0.14.0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-globalThis.SATSIM_VERSION = "0.15.0";
+globalThis.SATSIM_VERSION = "0.14.0";
 
 export { default as Universe } from './engine/Universe.js'
 export { default as Event } from './engine/event/Event.js'
@@ -37,6 +37,7 @@ export { createViewer, mixinViewer } from './widgets/Viewer.js'
 export { fetchTle, parseTle } from './io/tle.js'
 export { southEastZenithToAzEl, spaceBasedToAzEl } from './engine/dynamics/gimbal.js'
 export { normalizeAxisSlewConfig, stepSlewAxis } from './engine/dynamics/slew.js'
+export { cloneWaypointRoute, compileWaypointRoute, evaluateWaypointRouteState } from './engine/dynamics/waypoint.js'
 export { getVisibility } from './engine/geometry/visibility.js'
 export { getShadowStatus, ShadowState } from './engine/geometry/shadow.js'
 
